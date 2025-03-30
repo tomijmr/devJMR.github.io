9e1606e40b73c719b98b8d5bc25575b1874f7676
Tomás Canavidez <tms1csgo@gmail.com>
2025-03-30 20:03:38 -0300

    $$s

--- sams-lite/dbconnect.php
+++ sams-lite/dbconnect.php
@@ -2,15 +2,15 @@
 
 // // # Conexión a la base de datos
 
-$servername = "localhost";
-$username = "root";  // Cambia por tu usuario de la base de datos
-$password = "";  // Cambia por tu contraseña
-$dbname = "sams-cotizador";
-
 // $servername = "localhost";
-// $username = "c2611613";  // Cambia por tu usuario de la base de datos
-// $password = "SI42dakize";  // Cambia por tu contraseña
-// $dbname = "c2611613_devjmr";
+// $username = "root";  // Cambia por tu usuario de la base de datos
+// $password = "";  // Cambia por tu contraseña
+// $dbname = "sams-cotizador";
+
+$servername = "localhost";
+$username = "c2611613";  // Cambia por tu usuario de la base de datos
+$password = "SI42dakize";  // Cambia por tu contraseña
+$dbname = "c2611613_devjmr";
 
 // Crear la conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
